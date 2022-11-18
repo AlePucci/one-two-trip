7ed5e9c18c99ad50a33c64c81e47d6ec337241fd
Alessio Disarò <alessiobesnate@gmail.com>
2022-11-18 19:58:51 +0100

    Basic Trip Layout
    Basic Trip Layout with mockup activities for reference

---
+++ app/src/main/java/it/unimib/sal/one_two_trip/Trip.java
@@ -0,0 +1,90 @@
+package it.unimib.sal.one_two_trip;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+
+public class Trip extends AppCompatActivity {
+    private ActivityResultLauncher<String[]> multiplePermissionLauncher;
+
+    private final String[] PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+    };
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Ask for permissions
+        ActivityResultContracts.RequestMultiplePermissions multiplePermissionsContract = new ActivityResultContracts.RequestMultiplePermissions();
+
+        multiplePermissionLauncher = registerForActivityResult(multiplePermissionsContract, isGranted -> {
+            if(!isGranted.containsValue(false)) {
+                loadMap();
+            }
+        });
+
+        setContentView(R.layout.activity_trip);
+
+        //Map setup
+        loadMap();
+        mapView = findViewById(R.id.mapView);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setMultiTouchControls(true);
+        mapView.setBuiltInZoomControls(false);
+        //TODO: draw the travel route
+        //TODO: zoom in the trip next activity
+        IMapController mapController = mapView.getController();
+        mapController.setZoom(9.5);
+        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
+        mapController.setCenter(startPoint);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    private void loadMap() {
+        boolean permissionsStatus = true;
+
+        for(String p: PERMISSIONS) {
+            permissionsStatus &= ActivityCompat.checkSelfPermission(this, p) == PackageManager.PERMISSION_GRANTED;
+        }
+
+        if(permissionsStatus) {
+            //Get the Map
+            Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+            Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
+        } else {
+            multiplePermissionLauncher.launch(PERMISSIONS);
+        }
+    }
+}
