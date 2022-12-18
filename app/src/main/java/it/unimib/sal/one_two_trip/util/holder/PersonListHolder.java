85bf16335e859c8c16a8678ac77f8969f62f577b
Alessandro Pucci <a.pucci6@campus.unimib.it>
2022-12-18 19:57:52 +0100

    Add empty constructor

--- app/src/main/java/it/unimib/sal/one_two_trip/util/holder/PersonListHolder.java
+++ app/src/main/java/it/unimib/sal/one_two_trip/util/holder/PersonListHolder.java
@@ -1,5 +1,7 @@
 package it.unimib.sal.one_two_trip.util.holder;
 
+import androidx.room.Ignore;
+
 import java.util.List;
 
 import it.unimib.sal.one_two_trip.model.Person;
@@ -8,9 +10,21 @@ import it.unimib.sal.one_two_trip.model.Person;
  * This class is used to store the list of persons in the database.
  */
 public class PersonListHolder {
-    public final List<Person> personList;
+    public List<Person> personList;
 
+    public PersonListHolder() {
+    }
+
+    @Ignore
     public PersonListHolder(List<Person> personList) {
+        this.personList = personList;
+    }
+
+    public List<Person> getPersonList() {
+        return personList;
+    }
+
+    public void setPersonList(List<Person> personList) {
         this.personList = personList;
     }
 }
