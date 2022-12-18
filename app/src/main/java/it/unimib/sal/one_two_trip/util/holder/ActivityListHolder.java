04ed6fd26b2d50dce81adca70e4251641007db5d
Alessandro Pucci <a.pucci6@campus.unimib.it>
2022-12-18 19:57:52 +0100

    Add empty constructor

--- app/src/main/java/it/unimib/sal/one_two_trip/util/holder/ActivityListHolder.java
+++ app/src/main/java/it/unimib/sal/one_two_trip/util/holder/ActivityListHolder.java
@@ -1,5 +1,7 @@
 package it.unimib.sal.one_two_trip.util.holder;
 
+import androidx.room.Ignore;
+
 import java.util.List;
 
 import it.unimib.sal.one_two_trip.model.Activity;
@@ -8,9 +10,21 @@ import it.unimib.sal.one_two_trip.model.Activity;
  * This class is used to store the list of activities in the database.
  */
 public class ActivityListHolder {
-    public final List<Activity> activityList;
+    public List<Activity> activityList;
 
+    public ActivityListHolder() {
+    }
+
+    @Ignore
     public ActivityListHolder(List<Activity> activityList) {
+        this.activityList = activityList;
+    }
+
+    public List<Activity> getActivityList() {
+        return activityList;
+    }
+
+    public void setActivityList(List<Activity> activityList) {
         this.activityList = activityList;
     }
 }
