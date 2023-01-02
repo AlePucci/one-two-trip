4f9158c6c094b4860696cce38fb1cdf093c8aeda
Alessio Disarò <alessiobesnate@gmail.com>
2023-01-02 10:36:51 +0100

    Removed Not Used Method

--- app/src/main/java/it/unimib/sal/one_two_trip/ui/trip/activity/ActivityLocationEditFragment.java
+++ app/src/main/java/it/unimib/sal/one_two_trip/ui/trip/activity/ActivityLocationEditFragment.java
@@ -6,33 +6,21 @@ import static it.unimib.sal.one_two_trip.util.Constants.SHARED_PREFERENCES_FILE_
 
 import android.app.Application;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.Navigation;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.google.android.material.button.MaterialButton;
-import com.google.android.material.materialswitch.MaterialSwitch;
-import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.switchmaterial.SwitchMaterial;
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
-
-import java.util.Date;
 
 import it.unimib.sal.one_two_trip.R;
 import it.unimib.sal.one_two_trip.model.Activity;
@@ -172,11 +160,5 @@ public class ActivityLocationEditFragment extends Fragment {
                         .getMessage()), Snackbar.LENGTH_SHORT).show();
             }
         });
-    }
-
-    private boolean hasLocationChanged(TextInputLayout location) {
-        return location.getEditText() != null &&
-                !location.getEditText().getText().toString().equals("") &&
-                !location.getEditText().getText().toString().equals(activity.getLocation());
     }
 }
