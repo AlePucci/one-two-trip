35ed69f74876afade38f6b3ec451b3e786963c7f
Alessandro Pucci <a.pucci6@campus.unimib.it>
2023-02-11 12:42:29 +0100

    Change navigationBarColor to black in WelcomeActivity Make that clicking outside a field unfocus the corrent field in WelcomeActivity

--- app/src/main/java/it/unimib/sal/one_two_trip/ui/welcome/WelcomeActivity.java
+++ app/src/main/java/it/unimib/sal/one_two_trip/ui/welcome/WelcomeActivity.java
@@ -1,8 +1,16 @@
 package it.unimib.sal.one_two_trip.ui.welcome;
 
+import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import it.unimib.sal.one_two_trip.R;
 
@@ -15,5 +23,25 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.black));
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(@NonNull MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
     }
 }
+
